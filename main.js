cccc28c5bde489a1f8c98caa051c8e805ee4e7bd
oEmanuelFirmino <122304402+oEmanuelFirmino@users.noreply.github.com>
2023-04-13 23:38:42 -0300

    5

--- main.js
+++ main.js
@@ -11,10 +11,14 @@ function gerarPdf() {
 
     // Texto que deve estar no PDF
     doc.text("Comprovante de pagamento", 70, 10);
-    doc.text(nomePagador, 10, 30);
-    doc.text(nomeRecebedor, 10, 40);
-    doc.text(valor, 10, 50);
-    doc.text(data, 10, 60);
+    doc.text("Pago por: ", 10, 30);
+    doc.text(nomePagador, 36, 30);
+    doc.text("Recebido por: ", 10, 40);
+    doc.text(nomeRecebedor, 47, 40);
+    doc.text("Valor pago: R$", 10, 50);
+    doc.text(valor, 50, 50);
+    doc.text("Pago em: ", 10, 60);
+    doc.text(data, 37, 60);
     // Gerar PDF
     doc.save('comprovante.pdf');
 }
