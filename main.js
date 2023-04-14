a5e3cbf50b88e2554d2d47fbf9d3821c630c17ee
oEmanuelFirmino <122304402+oEmanuelFirmino@users.noreply.github.com>
2023-04-13 22:52:06 -0300

    1

---
+++ main.js
@@ -0,0 +1,21 @@
+const nomePagador = document.querySelector("#nomePagador").value
+const nomeRecebedor = document.querySelector("#nomeRecebedor").value
+const data = document.querySelector("#data").value.toString()
+const valor = document.querySelector("#valor").value
+
+function gerarPdf() {
+
+    var doc = new jsPDF();
+
+    // Texto que deve estar no PDF
+    doc.text("Comprovante de pagamento", 70, 10);
+    doc.text(nomePagador, 10, 30);
+    doc.text(nomeRecebedor, 10, 40);
+    doc.text(valor, 10, 50);
+    doc.text(data, 10, 60);
+
+    doc.
+
+        // Gerar PDF
+        doc.save('comprovante.pdf');
+}
